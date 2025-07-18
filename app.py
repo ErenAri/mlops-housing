134229512831a2c1e7f3684d854415e803b2dbac
ErenAri <erenari27@gmail.com>
2025-07-19 00:04:37 +0300

    add model auto-download from Google Drive

--- app.py
+++ app.py
@@ -5,24 +5,22 @@ import os
 import urllib.request
 
 if not os.path.exists("model.pkl"):
-    url = "https://drive.google.com/file/d/1RiNpIGiIpA-QbL7tpwmYASyfyBZX8E_B/view?usp=sharing"
+    url = "https://drive.google.com/uc?export=download&id=1RiNpIGiIpA-QbL7tpwmYASyfyBZX8E_B"
     urllib.request.urlretrieve(url, "model.pkl")
 
-
 app = Flask(__name__)
-
 model = joblib.load("model.pkl")
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "ML Ops Housing API is running!"
+    return "ML Ops API is running."
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json(force=True)
-    input_data = np.array(data['features']).reshape(1, -1)
+    input_data = np.array(data["features"]).reshape(1, -1)
     prediction = model.predict(input_data)
-    return jsonify({'prediction': prediction.tolist()})
+    return jsonify({"prediction": prediction.tolist()})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
